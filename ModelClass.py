8c66e7877438c958328e8c015c8a4c914ee572be
Rahil-Essani <86767796+Rahil-Essani@users.noreply.github.com>
2022-01-04 19:04:27 +0500

    implemented Register Route

--- ModelClass.py
+++ ModelClass.py
@@ -15,28 +15,49 @@ class Model:
             print("There is error in connection.", str(e))
 
     def __del__(self):
-        if self.connection!= None:
+        if self.connection is not None:
             self.connection.close()
 
-    def dml_run(self,query,args):
+    def dml_run(self,query,args,type):
         data = None
+        flag = False
         try:
-            if self.connection != None:
+            if self.connection is not None:
                 cursor = self.connection.cursor(pymysql.cursors.DictCursor)
                 cursor.execute(query,args)
-                data = cursor.fetchall()
+                if type == 'get':
+                    data = cursor.fetchall()
+                elif type == 'insert':
+                    flag = True
         except Exception as e:
             print("Exception in CheckUserExist.", str(e))
         finally:
             if cursor is not None:
                 cursor.close()
-            return data
+            if type == 'get': 
+                return data
+            elif type == 'insert':
+                self.connection.commit()
+                return flag
             
     def login(self, email, password):
         query = "select * from users where email=%s AND user_pass=%s"
         args = (email,password)
         user_list = None
-        user_list = self.dml_run(query,args)
-        if user_list != None:
-            return user_list
-        return False
+        user_list = self.dml_run(query,args,'get')
+        return user_list if (bool(user_list)) else False
+
+    def user_exist(self, email):
+        query = "select * from users where email = %s"
+        args = email
+        user_data = None
+        user_data = self.dml_run(query,args, 'get')
+        return True if (bool(user_data)) else False
+    
+    def register(self, new_user):
+        query = 'INSERT INTO users VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        args = (new_user["username"],new_user["email"],new_user["user_pass"],new_user["user_status"],
+        new_user["date_joined"],new_user["dob"],new_user["gender"],new_user["location"],new_user["address"],
+        new_user["profile_picture"])
+        success = self.dml_run(query,args,'insert')
+        return True if (success == True) else False
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from logging import DEBUG
-from pymysql import connections
+from sys import modules
+from pymysql import NULL, connections
 from ModelClass import *
 from flask_cors import CORS
+from datetime import datetime
 from flask import Flask, request, jsonify, session
 
 app = Flask(__name__)
@@ -9,17 +11,44 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.secret_key = 'dailykidiary'
 
-@app.route('/users',methods = ["post"])
-def user_access():
+@app.route('/login',methods = ["post"])
+def login_user():
     connection = Model("localhost", "root", "1234", "dailykidiary")
     email = request.form.get('email')
     password = request.form.get('pass')
     user_data = connection.login(email,password)
     if len(user_data) > 0:
         session["email"] = user_data[0]['email']
-        print(session['email'])
-    return jsonify(user_data)
+        user_data[0]['user_status'] = True
+        return jsonify(user_data)
+    return jsonify(NULL)
 
+@app.route('/register')
+def register_user():
+    connection = Model("localhost", "root", "1234", "dailydiary")
+    now = datetime.now()
+    new_user = dict()
+    new_list = list()
+    new_user["username"] = request.form.get("fname") + " " + request.form.get("lname")
+    new_user["email"] = request.form.get('email')
+    new_user["user_pass"] = request.form.get('pass')
+    new_user["user_status"] = True
+    new_user["date_joined"] = now.strftime('%d-%m-%Y')
+    new_user["dob"] = request.form.get('dob')
+    new_user["gender"] = request.form.get('gen')
+    new_user["location"] = request.form.get('loc')
+    new_user["address"] = request.form.get('add')
+    new_user["profile_picture"] = request.form.get('img')
+    if connection.user_exist(new_user["email"]) is False:
+        connection.register(new_user)
+        session["email"] = new_user['email']
+        new_list.append(new_user)
+        return jsonify(new_list)
+    return jsonify(NULL)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+   
+    
+    
