85435029810db682338bb3afef65fb5dcd3b08d0
MuhammadTalhaTahir <bitf19a003@pucit.edu.pk>
2022-01-04 16:45:08 +0500

    session added

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from logging import DEBUG
 from pymysql import connections
 from ModelClass import *
 from flask_cors import CORS
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.secret_key = 'dailykidiary'
 
 @app.route('/users',methods = ["post"])
 def user_access():
@@ -14,6 +15,9 @@ def user_access():
     email = request.form.get('email')
     password = request.form.get('pass')
     user_data = connection.login(email,password)
+    if len(user_data) > 0:
+        session["email"] = user_data[0]['email']
+        print(session['email'])
     return jsonify(user_data)
 
 
